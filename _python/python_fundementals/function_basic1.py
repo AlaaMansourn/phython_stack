59be6410d2ba659df446406ade0b4e9f4bf819ac
Alaamansourn <mansouralaa887@gmail.com>
2021-05-05 23:40:16 +0300

    comment

---
+++ _python/python_fundementals/function_basic1.py
@@ -0,0 +1,30 @@
+##for x in range(0,150):
+   ## print(x)
+
+##for x in range(0,150):
+   ## if(x%5==0):
+     ## print(x)
+#for x in range(1,101):
+ ##   if(x%10==0):
+        ##print("coding dojo")
+    ##elif(x%5==0):
+       ##  print("coding")  
+    ##else:
+       ## print(x)
+##sum=0
+##for x in range(0,500000):
+   # if(x%2==1):
+      #  sum+=x
+    
+#print(sum)
+
+##for x in range(2018,0,-4):
+      ##  print(x)
+
+lowNum=2
+HighNum=9
+Mult=3
+for u in range(lowNum+1,HighNum+1,Mult):
+    if(lowNum%Mult):
+        print(u)
+
