a6312c9d3ba00dd894867b530d59848fc8644fbd
Alaamansourn <mansouralaa887@gmail.com>
2021-05-06 01:44:48 +0300

    secnond assigment

---
+++ _python/python_fundementals/basic_function 2.py
@@ -0,0 +1,75 @@
+##def a():
+##return 5 
+##print(a())
+
+##5
+
+##--------------
+##def a():
+    ##return 5
+##print(a()+a())
+
+##def a():
+   ## return 5
+   ## return 10
+##print(a())
+
+##5
+##-----------------
+##def a():
+  ##  return 5
+  ##  print(10)
+##print(a())
+##10
+##10
+##-----------------------------
+##def a(b,c):
+   ## print(b+c)
+##print(a(1,2) + a(2,3))
+##3
+##5
+# ##-----------------------------
+# def a():
+#     b = 100
+#     print(b)
+#     if b < 10:
+#         return 5
+#     else:
+
+##b = 500
+##print(b)
+##def a():
+  ##  b = 300
+    ##print(b)
+    ##return b
+#print(b)
+#a()
+#print(b)
+500
+500
+300
+500
+-------------------------------
+def a():
+    print(1)
+    b()
+    print(2)
+def b():
+    print(3)
+a()
+-------------------------------
+def a():
+    print(1)  
+    x = b()  //  #x=5
+    print(x)  
+    return 10
+def b():
+    print(3)
+    return 5
+y = a()
+print(y)
+
+1
+3
+5
+10
