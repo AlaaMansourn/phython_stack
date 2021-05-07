ae093323556b5065de0207e684c1a2091ef47abe
Alaamansourn <mansouralaa887@gmail.com>
2021-05-07 15:37:14 +0300

    basic function 2

---
+++ _python/python_fundementals/basic_function2.py
@@ -0,0 +1,39 @@
+##def CountDown(num):
+  ##  for i in range(num,0,-1):
+      ##  print(i);
+
+##CountDown(9)
+
+#def printFirst(list):
+   # print(list[0])
+   # return list[1]
+
+#x=printFirst([1,2])
+#print(x)
+
+#def FirstPlus(list):
+    #return list[0]+len(list)
+
+#x=FirstPlus([1,2,3,4,8,9])
+#print(x)
+
+#def greaterSecond(list):
+    #list2=[]
+    #for i in range(0,len(list)):
+        #if(list[i]>list[1]):
+           # list2.append(list[i])
+    
+    #return list2
+
+#def createList(l,v):
+    #list=[]
+    #for i in range(0,l):
+       # list.append(v)
+    
+    #return list
+
+c=createList(8,9)
+print(c)
+
+
+
