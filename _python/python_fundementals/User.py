805f086404e0a98d8322b85f7750bde461d28eee
Alaamansourn <mansouralaa887@gmail.com>
2021-05-07 13:58:56 +0300

    my assigment

---
+++ _python/python_fundementals/User.py
@@ -0,0 +1,39 @@
+class User:
+    def __init__(self,balance,name): #this is the constructor
+        self.balance = balance
+        self.name=name
+
+
+    def make_withdrawal(self,amount): #this is the method 1
+        
+        self.balance = self.balance-amount
+    
+    def make_deposit(self,amount):
+        self.balance=self.balance+amount
+    
+
+
+    
+    def display_user_balance(self): 
+        #this is the method 2
+       ## print("Wirtdraw amount" , self.amount)
+        print("Remaining balance" , self.balance,"name",self.name)
+
+
+if __name__ == "__main__":
+    user1 = User(500,"alaa")
+    user2 = User(10000,"mohmmad")
+    user3 = User(3000,"sami")
+
+user1.make_deposit(1000)
+##user1.make_deposit(300)
+#user1.make_deposit(400)
+user1.make_withdrawal(300)
+
+user2.make_deposit(5000)
+user2.make_deposit(5000)
+user2.make_withdrawal(100)
+user2.make_withdrawal(300)
+
+
+user1.display_user_balance()
---
+++ _python/python_fundementals/hello_world.py
@@ -0,0 +1,21 @@
+
+print("Hello_World")
+
+
+name="alaa"
+print("hello",name)
+
+print("hello"+name)
+
+
+
+num =3
+print("hello",num)
+
+print("hello"+str(3))
+
+food1="rice"
+food2="cici"
+
+print("i love to eat to eat {f1} and {f2}".format(f1=food1,f2=food2))
+print(f"i love to eat {food1} and {food2}")
