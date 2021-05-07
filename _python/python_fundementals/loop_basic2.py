d28e07c561f8f7614edc25a52a41cb36c6f45a88
Alaamansourn <mansouralaa887@gmail.com>
2021-05-07 23:25:13 +0300

    mine

---
+++ _python/python_fundementals/loop_basic2.py
@@ -0,0 +1,120 @@
+#def BiggieSize(list):
+  #  for i in range(1,len(list)):
+     #   if(list[i]>0):
+         #   list[i]="big"
+    #return list
+
+#x=BiggieSize([1,2,3,4,5])
+#print(x)
+#def CountPositives(list):
+    #count=0
+    #for i in range(0,len(list)):
+       # if(list[i]>0):
+           # count+=1
+    
+    #list[len(list)-1]=count
+    #return list
+
+#x= CountPositives([-1,3,4,5,9,2])
+#print(x)
+
+#def SumTotal(list):
+  #  sum=0
+  #  for i in range(0,len(list)):
+      #  sum+=list[i]
+    #return sum 
+
+#x=SumTotal([-1,5,6,-2])
+#print(x)
+
+#def Average(list):
+  #  sum = 0
+  #  avg = 0
+   # for i in range(0,len(list)):
+       # sum+=list[i]
+    
+    #avg = sum/len(list)
+   # return avg
+
+#x=Average([1,2,3,4])
+#print(x)
+#def length(list):
+   # count = 0
+    #for i in range(0,len(list)):
+    #    count+=1
+
+    #return count
+
+#x=length([1,2,4,5,6])
+#print(x)
+
+#def Minimum(list):
+   # min=list[0]
+    #if(len(list)==0):
+      #  return False
+    #else:
+       # for i in range(0,len(list)):
+          #  if(list[i]<min):
+          #      min=list[i]
+    
+
+   # return min 
+
+#x=Minimum([4#,2,3,2,7,2])
+#print(x)
+
+
+#def Maxmum(list):
+   # max=list[0]
+   # if(len(list)==0):
+      # return False
+   # else:
+     #  for i in range(0,len(list)):
+            #if(list[i]>max):
+             #  max=list[i]
+    
+
+    #return max
+
+#x=Maxmum([4,2,3,2,7,2])
+#print(x)
+
+#def UltimateAnalysis(list):
+  #  dictionary={}
+   # max=list[0]
+    #min=list[0]
+    #sum=0
+    #avg=0
+    #count=0
+
+    #for i in range(0,len(list)):
+      #  if(list[i]<min):
+         #   min=list[i]
+        
+       # if(list[i]>max):
+         #   max=list[i]
+
+       # count+=1
+       # sum+=list[i]
+
+
+   # avg = sum/len(list)
+   # return {'sum_total':sum,'average':avg,'minimum':min,'maximum':max,'length':count}
+
+#x=UltimateAnalysis([1,2,4,8,9])
+#print(x)
+
+#def Reverselist(list):
+  #  for i in range(0,int(len(list)/2)):
+     #   list[i], list[len(list)-1-i] = list[len(list)-1-i], list[i]
+    
+    #return list
+
+#x=Reverselist([1,2,3,4])
+#print(x)
+
+
+
+            
+        
+
